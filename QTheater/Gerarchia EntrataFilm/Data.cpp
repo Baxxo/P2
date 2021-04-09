b771f446a7537f5f72f3510551e8ee5cc99c39b8
Baxxo <matteobasso9@gmail.com>
2021-04-09 20:18:24 +0200

    Update Data.cpp

--- QTheater/Gerarchia EntrataFilm/Data.cpp
+++ QTheater/Gerarchia EntrataFilm/Data.cpp
@@ -5,7 +5,7 @@ using std::string;
 
 Data::Data(int gg, int mm, int aa)
 
-	:giorno{gg}, mese{mm}, anno{aa}
+	:giorno(gg), mese(mm), anno(aa)
 	
 	{}
 
@@ -22,5 +22,5 @@ int Data::getAnno() const{
 }
 
 string Data::ToString() const {
-    return std::to_string(giorno) + "/" + std::to_string(mese) + "/" + std::to_string(anno);
+	return giorno + "/" + mese + "/" + anno;
 }
