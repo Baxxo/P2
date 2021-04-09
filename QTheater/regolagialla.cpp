33ba935b94c30134297fca8723267317b7f57452
Baxxo <matteobasso9@gmail.com>
2021-04-09 20:14:07 +0200

    Update regolagialla.cpp

--- QTheater/regolagialla.cpp
+++ QTheater/regolagialla.cpp
@@ -5,8 +5,9 @@ RegolaGialla::RegolaGialla(unsigned int dP, bool m): Regola(dP,m)
 
 }
 
-void RegolaGialla::getRegole(unsigned int &distP, bool &m)
+void RegolaGialla::getRegole(unsigned int &distP, bool &m, bool c= true);
 {
     distP = getDistanzaPosti();
     m = getMustMascherina();
+    c = getCongiuntiVicini();
 }
