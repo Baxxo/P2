c9fca32bafcda026872cda30bf3238c539734b68
Baxxo <matteobasso9@gmail.com>
2021-04-09 20:14:20 +0200

    Update regolagialla.h

--- QTheater/regolagialla.h
+++ QTheater/regolagialla.h
@@ -6,7 +6,7 @@ class RegolaGialla: public Regola
 {
 public:
     RegolaGialla(unsigned int dP=1, bool m=true);
-    void getRegole(unsigned int& distP, bool& m);
+    void getRegole(unsigned int& distP, bool& m, bool c= true);
 };
 
 #endif // REGOLAGIALLA_H
