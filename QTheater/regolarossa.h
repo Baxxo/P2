15c7df4b4032924e4017e85fedfe366ef753939d
Baxxo <matteobasso9@gmail.com>
2021-04-09 20:22:35 +0200

    Update regolarossa.h

--- QTheater/regolarossa.h
+++ QTheater/regolarossa.h
@@ -6,7 +6,7 @@ class RegolaRossa : public Regola
 {
 public:
     RegolaRossa(unsigned int dP=5, bool m=true, bool c= false);
-    void getRegole(unsigned int& distP, bool& m);
+    void getRegole(unsigned int& distP, bool& m, bool c= true);
 };
 
 #endif // REGOLAROSSA_H
