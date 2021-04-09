e13591cbca4424fb159649477f50aef38682b8b9
Baxxo <matteobasso9@gmail.com>
2021-04-09 20:13:09 +0200

    Update regolabianca.cpp

--- QTheater/regolabianca.cpp
+++ QTheater/regolabianca.cpp
@@ -5,8 +5,9 @@ RegolaBianca::RegolaBianca(unsigned int dP, bool m, bool c): Regola(dP,m,c)
 
 }
 
-void RegolaBianca::getRegole(unsigned int &distP, bool &m)
+void RegolaBianca::getRegole(unsigned int &distP, bool &m, bool &c)
 {
     distP = getDistanzaPosti();
     m = getMustMascherina();
+    c = getCongiuntiVicini();
 }
