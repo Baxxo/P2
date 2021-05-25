4487e38a65495a05fff677998a1aab4628463b86
Baxxo <matteobasso9@gmail.com>
2021-05-25 22:03:13 +0200

    Update MyVector.h

--- MyVector.h
+++ MyVector.h
@@ -1,39 +1,156 @@
 #ifndef MYVECTOR_H
 #define MYVECTOR_H
 
+#include <iostream>
+using std::cout;
+using std::endl;
+
 template <class T>
 class MyVector
 {
 private:
-    T* v;
-    unsigned int size; //numero celle popolatr
-    unsigned int capacity; //dimensione totale
-    class Iterator
-    {
-        friend class MyVector;
-    public:
-        T* ptr;//DeepPtr<T>* ptr;
-        bool pastTheEnd;
-        Iterator(T* p, bool pte=false);
-        Iterator();
-        T* operator ->() const;
-        T& operator * () const;
-        Iterator& operator++();
-        Iterator& operator--();
-        bool operator == (const Iterator& o) const;
-        bool operator != (const Iterator& o) const;
-    };
+  T* v;
+  unsigned int size; //numero celle popolate
+  unsigned int capacity; //dimensione totale
+
 public:
-    MyVector(T* p, unsigned int sz=0);
-    ~MyVector();
-    MyVector(const MyVector& o);
-    unsigned int getSize() const;
-    T &operator[](unsigned int i) const;
 
-    Iterator begin();
-    Iterator end();
+  class Iterator
+  {
+    friend class MyVector;
+    private:
+      T* ptr;//DeepPtr<T>* ptr;
+      bool pastTheEnd;
 
-    bool add(const T& o);
+    public:
+      Iterator(T* p, bool pte=false): ptr(p), pastTheEnd(pte){}
+      Iterator(): ptr(nullptr), pastTheEnd(false){}
+
+      T* operator ->() const {return ptr;}
+
+      T& operator * () const {return *ptr;}
+
+      Iterator& operator++(){
+        if(ptr!= nullptr)
+          {
+              if(!pastTheEnd)
+              {
+                  if(ptr+1==nullptr)
+                  {
+                      ++ptr;
+                      pastTheEnd=true;
+                  }
+                  else
+                  {
+                      ptr=ptr+1;
+                  }
+              }
+          }
+          return *this;
+      }
+      Iterator& operator--()
+      {
+          if(ptr!=nullptr)
+          {
+              if(pastTheEnd)
+              {
+                  --ptr;
+                  pastTheEnd=false;
+              }
+              else
+                  ptr=ptr-1;
+          }
+          return *this;
+      }
+      bool operator == (const Iterator& o) const{ return o.ptr == ptr;}
+      bool operator != (const Iterator& o) const{ return o.ptr != ptr;}
+  };
+
+  MyVector(T* p=nullptr, unsigned int sz=0): v(new T[(sz>=0?sz:0)]), size(sz), capacity(sz)
+  {
+    if(p == nullptr){
+        for(unsigned int i=0;i<sz;i++){
+            //cout << "if i: " << i << endl;
+            v[i] = nullptr;
+          }
+      }else{
+        for(unsigned int i=0;i<sz;i++){
+            //cout << "else i: " << i << endl;
+            v[i] = *p;
+          }
+      }
+  }
+  ~MyVector()
+  {
+    delete[] v;
+  }
+
+  MyVector(const MyVector& o);
+
+  unsigned int getSize() const
+  {
+    return size;
+  }
+
+  Iterator begin()
+  {
+    return v[0];
+  }
+  Iterator end()
+  {
+    return v[size-1];
+  }
+
+  T& operator[](unsigned int i) const{
+    return v[i];
+  }
+
+  bool add(const T* o)  {
+    if(o != nullptr){
+        //cout << "Capacity: " << capacity << " " << size<<endl;
+        if(size >= capacity){
+            //cout << " punto a" << endl;
+
+            //devo aumentare la dimensione dell'array
+            T* temp = new T[size*2+1];
+
+            //cout << " punto b size: "<<size*2+1 << endl;
+            unsigned int i=0;
+            for (; i < size; i++) {
+                temp[i] = v[i];
+              }
+            //aggiungo l' elemento
+            temp[i] = *o;
+            v = temp;
+            size = i+1;
+            capacity = i*2+1;
+            //cout << " punto c inserito" << endl;
+            //cout << "size: " << size << " capacity: " << capacity << endl;
+            return true;
+          }else{
+            //cout << "Sono qui" << endl;
+            //aggiungo l' elemento
+            v[size++] = *o;
+            //cout << "size: " << size << " capacity: " << capacity << endl;
+            return true;
+          }
+        //cout << "NON inserito" << endl;
+      }
+    return false;
+  }
+
+  void remove(/*const*/ T& o){
+      for (unsigned int i = 0; i < size; ++i) {
+          if(v[i] == o ){
+              //delete *v[j];
+              for(unsigned int j=i;j<size-1;j++){
+                  v[j] = v[j+1];
+                }
+              size--;
+              return;
+            }
+        }
+  }
 };
 
 #endif // MYVECTOR_H
