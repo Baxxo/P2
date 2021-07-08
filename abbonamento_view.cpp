ed4d5d3432b1777ae5d3b7699632bfe402a5f49f
student <matteobasso9@gmail.com>
2021-07-08 17:02:12 +0200

    add radioButton abbonamento

--- abbonamento_view.cpp
+++ abbonamento_view.cpp
@@ -1,6 +1,16 @@
 #include "abbonamento_view.h"
 
 Abbonamento_view::Abbonamento_view(Controller *c, QWidget *parent)
-    : QWidget(parent), controller(c) {}
+    : QWidget(parent),
+      controller(c),
+      tipoAbbonamento(new QGroupBox),
+      layoutRadio(new QHBoxLayout),
+      abbonamento(new QRadioButton("Abbonamento")),
+      abbonamentoFamigliare(new QRadioButton("Abbonamento Famigliare")) {
+  abbonamento->setChecked(true);
+  layoutRadio->addWidget(abbonamento);
+  layoutRadio->addWidget(abbonamentoFamigliare);
+  tipoAbbonamento->setLayout(layoutRadio);
+}
 
 void Abbonamento_view::resizeMe() { adjustSize(); }
--- abbonamento_view.h
+++ abbonamento_view.h
@@ -1,13 +1,21 @@
 #ifndef ABBONAMENTO_VIEW_H
 #define ABBONAMENTO_VIEW_H
 
+#include <QGroupBox>
+#include <QHBoxLayout>
+#include <QRadioButton>
 #include <QWidget>
+
 #include "mainwindow.h"
 
 class Abbonamento_view : public QWidget {
   Q_OBJECT
  private:
   Controller* controller;
+  QGroupBox* tipoAbbonamento;
+  QHBoxLayout* layoutRadio;
+  QRadioButton* abbonamento;
+  QRadioButton* abbonamentoFamigliare;
 
  private slots:
 
