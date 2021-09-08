15949b9695a9ffaa1869bfab2c1b4a99db5dd6d3
Baxxo <matteobasso9@gmail.com>
2021-09-08 11:45:12 +0200

    Delete regolabianca.cpp

--- regolabianca.cpp
+++
@@ -1,9 +0,0 @@
-#include "regolabianca.h"
-
-RegolaBianca::RegolaBianca(unsigned int dP, bool m)
-    : Regola(dP, m) {}
-
-void RegolaBianca::getRegole(unsigned int &distP, bool &m) {
-  distP = getDistanzaPosti();
-  m = getMustMascherina();
-}
