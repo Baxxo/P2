6d04b9e7a7019aae0925ff56a3b1698d552ccd99
Baxxo <matteobasso9@gmail.com>
2021-09-08 11:45:02 +0200

    Delete regolagialla.cpp

--- regolagialla.cpp
+++
@@ -1,9 +0,0 @@
-#include "regolagialla.h"
-
-RegolaGialla::RegolaGialla(unsigned int dP, bool m)
-    : Regola(dP, m) {}
-
-void RegolaGialla::getRegole(unsigned int &distP, bool &m) {
-  distP = getDistanzaPosti();
-  m = getMustMascherina();
-}
