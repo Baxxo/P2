e8c6e9660d52a5cbcd3c7b8231b13eb41af474ca
Baxxo <matteobasso9@gmail.com>
2021-09-08 11:45:20 +0200

    Delete regolaarancione.cpp

--- regolaarancione.cpp
+++
@@ -1,9 +0,0 @@
-#include "regolaarancione.h"
-
-RegolaArancione::RegolaArancione(unsigned int dP, bool m)
-    : Regola(dP, m) {}
-
-void RegolaArancione::getRegole(unsigned int &distP, bool &m) {
-  distP = getDistanzaPosti();
-  m = getMustMascherina();
-}
