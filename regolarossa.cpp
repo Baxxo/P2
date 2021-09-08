56b8b999aff67267ae24e946171d8f6d80958c61
Baxxo <matteobasso9@gmail.com>
2021-09-08 11:44:53 +0200

    Delete regolarossa.cpp

--- regolarossa.cpp
+++
@@ -1,8 +0,0 @@
-#include "regolarossa.h"
-
-RegolaRossa::RegolaRossa(unsigned int dP, bool m) : Regola(dP, m) {}
-
-void RegolaRossa::getRegole(unsigned int &distP, bool &m) {
-  distP = getDistanzaPosti();
-  m = getMustMascherina();
-}
